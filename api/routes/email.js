1e6928e3c36dd80acd6b7f293dbfdfa446399e43
Christina Sun <csun@bayviewglen.ca>
2024-07-06 20:03:28 -0400

    move emailjs key to .env

--- api/routes/email.js
+++ api/routes/email.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
@@ -5,10 +6,10 @@ const axios = require('axios');
 // send reminder email
 router.post("/reminder", async (req, res) => {
     let data = JSON.stringify({
-        "service_id": "service_ig0cejn",
+        "service_id": process.env.EMAILJS_SERVICEID,
         "template_id": "template_jh5mm8g",
-        "user_id": "YLwiiUfhC-PAfy-bz",
-        "accessToken": "e6Tq1NHcbt7rO4GG3iEBf",
+        "user_id": process.env.EMAILJS_USERID,
+        "accessToken": process.env.EMAILJS_ACCESSTOKEN,
         "template_params": {
             "to_name": req.body.name,
             "to_email": req.body.email,
@@ -39,10 +40,10 @@ router.post("/reminder", async (req, res) => {
 
 router.post("/update", async (req, res) => {
     let data = JSON.stringify({
-        "service_id": "service_ig0cejn",
+        "service_id": process.env.EMAILJS_SERVICEID,
         "template_id": "template_15luccf",
-        "user_id": "YLwiiUfhC-PAfy-bz",
-        "accessToken": "e6Tq1NHcbt7rO4GG3iEBf",
+        "user_id": process.env.EMAILJS_USERID,
+        "accessToken": process.env.EMAILJS_ACCESSTOKEN,
         "template_params": {
             "to_name": req.body.name,
             "to_email": req.body.email,
